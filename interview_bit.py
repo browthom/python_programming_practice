235ab1d986164f83a99ebf429fd91a669f58e430
Thomas Brown <browthom93@gmail.com>
2020-08-26 21:26:13 -0400

    Added some more functions for practice

---
+++ interview_bit.py
@@ -0,0 +1,70 @@
+# Coding Questions from InterviewBit website
+from math import floor
+
+
+def get_starting_gas_station(a, b):
+    # a -> amount of gas at gas station a[i]
+    # b -> b[i] is the amount of gas to get from i to i+1
+
+    gas_in_tank = 0
+
+    # Continually iterate through possible starting indices
+    for i in range(len(a)):
+        # j is the starting index
+        j = i
+        # Initial fill up
+        gas_in_tank = a[i]
+
+        # Continue on path until you aren't able to make
+        # it to the next gas station
+        while gas_in_tank >= b[j]:
+            # Burn gas traveling to a[j+1]
+            gas_in_tank -= b[j]
+
+            # Increment j accordingly
+            # Cycle around to index=0 if need be
+            if j < len(a)-1:
+                j += 1
+            else:
+                j = 0
+
+            # Fill up gas at a[j+1] gas station
+            gas_in_tank += a[j]
+
+            # If we have made it to starting point
+            # return index
+            if j == i:
+                return j
+
+    # No possible path found
+    return -1
+
+
+def find_majority_element(array):
+    array.sort()
+    count = 1
+    prev = array[0]
+
+    # An intuitive way to find the majority
+    # element is to first sort the numbers in the
+    # array incrementally. That way we can
+    # easily count the number of successive
+    # integers that are identical to each other in the array
+    for i in range(1, len(array)):
+        # count successive elements that are equal
+        if prev == array[i]:
+            count += 1
+        # if the previous element doesn't equal the current element
+        # then reset count as that element was not a majority element
+        else:
+            count = 0
+
+        # If we have found majority element, return element
+        if count >= floor(len(array)/2):
+            return array[i]
+
+        # rewrite 'prev' for next go-around
+        prev = array[i]
+
+    # No majority element in array
+    return -1
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import logic_1 as f1
-import logic_2 as f2
-
 # Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
-    print(f2.make_bricks(2, 1, 10))
+    a = [1, 1, 4]
+    b = [2, 2, 2]
+    c = [1, 2, 3, 3, 3, 3, 2]
+    #
+    # # print(get_starting_gas_station(a, b))
+    # print(find_majority_element(c))
 
